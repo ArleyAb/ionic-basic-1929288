2f96cd97c08fd3f33d645b4dbfb6f8cc49cd1ecf
Astrid <141696335+ArleyAb@users.noreply.github.com>
2023-10-09 18:10:47 -0600

    Practica 6 Final
    Updates

--- ionic-basic/src/app/app.module.ts
+++ ionic-basic/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { RouteReuseStrategy } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
+import { FloatMenuComponent } from './float-menu/float-menu.component';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [AppComponent],
+  declarations: [AppComponent, FloatMenuComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
--- ionic-basic/src/app/float-menu/float-menu.component.ts
+++ ionic-basic/src/app/float-menu/float-menu.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Menu } from 'src/app/interface/menu';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-float-menu',
@@ -6,9 +8,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./float-menu.component.scss'],
 })
 export class FloatMenuComponent  implements OnInit {
+  datosMenu: Menu[] =[
+    {nombre: 'home',enlace:'/home',
+    icono:'home-outline'},
+    {nombre: 'Alumnos',enlace:'/alumnos',
+  icono:'school-outline'},
+    {nombre: 'Receteas',enlace:'/receta',
+    icono:'restaurant-outline'},
+    {nombre: 'Presupuesto',enlace:'/presupuesto',
+    icono:'cash-outline'},
+    {nombre: 'inicio',enlace:'/inicio',
+    icono:'navigate-outline'},
+    {nombre: 'Tabs',enlace:'/tabs',
+    icono:'folder-outline'}
+  ];
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {}
 
+  navegar(link: string){
+    this.router.navigate([link]);
+  }
 }
